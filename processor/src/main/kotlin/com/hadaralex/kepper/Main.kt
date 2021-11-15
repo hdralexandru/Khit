c86424ded07f6c777fd1b40453bf1725f942408e
Alexandru Hadar <alex.hadar@tapptitude.com>
2021-11-15 20:26:23 +0200

    Moved products.xlsx to outer package

--- processor/src/main/kotlin/com/hadaralex/kepper/Main.kt
+++ processor/src/main/kotlin/com/hadaralex/kepper/Main.kt
@@ -1,10 +1,14 @@
 package com.hadaralex.kepper
 
+import com.hadaralex.kepper.tryout.XlsxOpener
 import com.hadaralex.kepper.util.logMe
 
 internal object Main {
+    private const val DATA_FILE_PATH: String = "data/products.xlsx"
+
     @JvmStatic
     fun main(args: Array<String>) {
         logMe { "Starting processor-main" }
+        XlsxOpener(DATA_FILE_PATH).open()
     }
 }
--- processor/src/main/kotlin/com/hadaralex/kepper/tryout/XlsxOpener.kt
+++ processor/src/main/kotlin/com/hadaralex/kepper/tryout/XlsxOpener.kt
@@ -1,10 +1,13 @@
 package com.hadaralex.kepper.tryout
 
+import com.hadaralex.kepper.util.logMe
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 class XlsxOpener(private val filename: String) {
     fun open() {
         val workbook: XSSFWorkbook = XSSFWorkbook(filename)
-
+        workbook.sheetIterator().forEach {
+            logMe { "Sheetname: ${it.sheetName}" }
+        }
     }
 }
